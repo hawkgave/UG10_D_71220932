2dd9ef33994e7cd6dc89caf53fe7068eeb8fb0eb
hawkgave <117901120+hawkgave@users.noreply.github.com>
2022-11-24 16:28:57 +0700

    Add files via upload

---
+++ 2_D_71220932.py
@@ -0,0 +1,4 @@
+nama = input("masukkan nama pemain pertama: ")
+nama = input("masukkan nama pemain kedua: ")
+nama = input("masukkan nama pemain ketiga: ")
+kartu=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
